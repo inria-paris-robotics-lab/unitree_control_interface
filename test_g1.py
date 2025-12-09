1026a971bc25dc80b8c7345f833e6fa12e06e502
earlaud <etienne.arlaud@inria.fr>
2025-12-09 11:35:57 +0100

    Use simple-mpc to test ID

--- test_g1.py
+++ test_g1.py
@@ -2,6 +2,14 @@ import rclpy
 from rclpy.node import Node
 from go2_control_interface_py.robot_interface import Go2RobotInterface
 from go2_description.loader import loadG1
+import pinocchio as pin
+import numpy as np
+from simple_mpc import (
+    RobotModelHandler,
+    RobotDataHandler,
+    KinodynamicsID,
+    KinodynamicsIDSettings,
+)
 
 
 class MyApp(
@@ -10,18 +18,68 @@ class MyApp(
     def __init__(self):
         Node.__init__(self, "my_app2")
 
-        self.robot = loadG1()
+        self.pin_robot_wrapper = loadG1()
 
         self.robot_if = Go2RobotInterface(self)
         self.robot_if.register_callback(self._sensor_reading_callback)
-        # self.viz = MeshcatVisualizer(self.robot.model, self.robot.collision_model, self.robot.visual_model)
+
+        # self.viz = MeshcatVisualizer(self.pin_robot_wrapper.model, self.pin_robot_wrapper.collision_model, self.pin_robot_wrapper.visual_model)
         # self.viz.initViewer(open=True)
         # self.viz.loadViewerModel()
         # self.viz.display(np.array([0,0,0, 0,0,0,1] + [0.] * 27))
 
+        # Create inverse dynamic
+        base_frame_name = "torso_link"
+        foot_points = np.array(
+            [[0.12, 0.03, -0.035], [-0.06, 0.03, -0.035], [-0.06, -0.03, -0.035], [0.12, -0.03, -0.035]]
+        )
+        self.model_handler = RobotModelHandler(self.pin_robot_wrapper.model, "half_sitting", base_frame_name)
+        self.model_handler.addQuadFoot("left_ankle_roll_link", base_frame_name, foot_points)
+        self.model_handler.addQuadFoot("right_ankle_roll_link", base_frame_name, foot_points)
+        self.data_handler = RobotDataHandler(self.model_handler)
+
+        kino_ID_settings = KinodynamicsIDSettings()
+        kino_ID_settings.kp_base = 7.0
+        kino_ID_settings.kp_posture = 100.0
+        kino_ID_settings.kp_contact = 10.0
+        kino_ID_settings.w_base = 100.0
+        kino_ID_settings.w_posture = 100.0  # 1.0
+        kino_ID_settings.w_contact_force = 0.001
+        kino_ID_settings.w_contact_motion = 1.0
+
+        self.kino_ID = KinodynamicsID(self.model_handler, 1.0 / 500.0, kino_ID_settings)
+
+        self.nq = self.model_handler.getModel().nq
+        self.nv = self.model_handler.getModel().nv
+
         # The robot will move by itself to the q_start configuration and wait for you first command
-        start_q = [0.0] * 27
-        self.robot_if.start_async(start_q)
+        self.robot_if.start_async(self.model_handler.getReferenceState()[7 : self.nq])
+
+    def _compute_base_pose_vel(self, q, dq):
+        model = self.model_handler.getModel()
+        data = self.data_handler.getData()
+        base_frame_id = model.getFrameId("torso_link")
+        foot_frame_id = model.getFrameId("left_ankle_roll_link")
+
+        q_full = pin.neutral(model)
+        q_full[7:] = q
+        dq_full = np.zeros(model.nv)
+        dq_full[6:] = dq
+
+        pin.forwardKinematics(model, data, q_full, dq_full)
+        pin.updateFramePlacements(model, data)
+
+        oMfoot = data.oMf[foot_frame_id]
+        oMbase = data.oMf[base_frame_id]
+
+        footMbase = oMfoot.actInv(oMbase)
+
+        w_v_foot = pin.getFrameVelocity(model, data, foot_frame_id, pin.ReferenceFrame.WORLD)
+        w_v_base = pin.getFrameVelocity(model, data, base_frame_id, pin.ReferenceFrame.WORLD)
+        w_v_foot_base = w_v_base - w_v_foot
+        v_foot_base = oMbase.actInv(w_v_foot_base)
+
+        return pin.SE3ToXYZQUAT(footMbase), v_foot_base.vector
 
     def _sensor_reading_callback(self, t, q, dq, ddq):
         # Reading timestamp, positions, velocities, accelerations
@@ -29,13 +87,18 @@ class MyApp(
         # self.viz.display(np.array([0,0,0, 0,0,0,1] + q))
 
         # Sending commands
-        q_des = [0.0] * 27
-        v_des = [0.0] * 27
-        tau_des = [0.0] * 27
+        q_des = self.model_handler.getReferenceState()[7 : self.nq]
+        v_des = [0.0] * (self.nv - 6)
         kp = ([150.0, 150.0, 50.0, 50.0] + [50.0] * 2) * 2 + [50.0] + ([50.0, 50.0, 50.0, 50.0] + [50.0] * 3) * 2
         kd = ([2.0, 2.0, 1.0, 1.0] + [1.0] * 2) * 2 + [1.0] + ([1.0, 1.0, 1.0, 1.0] + [1.0] * 3) * 2
+
+        base_pose, base_vel = self._compute_base_pose_vel(q, dq)
+
+        tau_cmd = self.kino_ID.solve(
+            t, np.concatenate((base_pose, np.array(q))), np.concatenate((base_vel, np.array(dq)))
+        )
         if self.robot_if.is_ready:
-            self.robot_if.send_command(q_des, v_des, tau_des, kp, kd)
+            self.robot_if.send_command(q_des, v_des, tau_cmd, kp, kd)
 
 
 def main(args=None):
